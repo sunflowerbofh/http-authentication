418764a5809c40cb1d397fc60b68cc8764674279
Katharina Drexel <katharina.drexel@bfh.ch>
2022-01-17 18:06:10 +0100

    Adding license header.
    Signed-off-by: Katharina Drexel <katharina.drexel@bfh.ch>

--- http-authentication.php
+++ http-authentication.php
@@ -8,6 +8,24 @@ Author: Daniel Westermann-Clark
 Author URI: http://danieltwc.com/
 */
 
+/* Copyright (C) 2011-2012 Daniel Westermann-Clark <daniel@danieltwc.com>
+
+SPDX-License-Identifier: GPL-2.0+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'options-page.php');
 
 class HTTPAuthenticationPlugin {
